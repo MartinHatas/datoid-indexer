4d22b555b27da11f09b2ebe15c540e96a89bfd96
Martin Hátaš <martin.hatas@gmail.com>
2021-04-26 13:05:24 +0000

    Initial commit

---
+++ src/main/java/io/hat/datoid/index/Application.java
@@ -0,0 +1,10 @@
+package io.hat.datoid.index;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
